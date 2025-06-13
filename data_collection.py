fdb2b95f6795538de7c0c92d9ea166f37042cd51
abs2 <albertbargallosales@gmail.com>
2025-06-13 18:19:14 +0200

    Feat: First working form

--- data_collection.py
+++ data_collection.py
@@ -1,7 +1,14 @@
 import streamlit as st
 from datetime import date
+import pymysql
+from dotenv import load_dotenv
+import os
 
 
+# Load environment variables
+load_dotenv(".env")
+
+# Set web app title
 st.title("ACL Recovery Tracker")
 
 # Form
@@ -14,3 +21,24 @@ with st.form("daily_log"):
     notes = st.text_area("Notes:")
     submit = st.form_submit_button("Submit")
 
+if submit:
+    with st.spinner("Saving to database..."):
+        # Save to db
+        with pymysql.connect(
+            host=os.environ["DB_ADDRESS"],
+            user=os.environ["DB_USER"],
+            password=os.environ["DB_PASS"],
+            port=int(os.environ["DB_PORT"]),
+            db=os.environ["DB_NAME"]
+        ) as mydb:
+            
+            cursor = mydb.cursor()
+
+            query = f"""
+                    INSERT INTO logs
+                    VALUES
+                    (%s, %s, %s, %s, %s, %s);
+                    """
+            cursor.execute(query, (log_date, pain, flexion, mood, rehab_done, notes))
+            mydb.commit()
+            st.write("Saved to database.")
